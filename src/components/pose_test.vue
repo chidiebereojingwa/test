6fc256ad163af2b8bf3becba2eb6fb5d5f269175
chidiebereojingwa <ojingwaoji@gmail.com>
2023-07-16 19:46:49 +0100

    Made modifications on post_test.vue

--- src/components/pose_test.vue
+++ src/components/pose_test.vue
@@ -1,130 +1,183 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { proto } from './proto_demo.mjs'
+import { ref, onMounted, reactive, watch } from "vue";
+import { proto } from "./proto_demo.mjs";
 
-const faces = ['face_up', 'face_down', 'right', 'left', 'vertical_up', 'vertical_down']
-
+const faces = [
+  "face_up",
+  "face_down",
+  "right",
+  "left",
+  "vertical_up",
+  "vertical_down",
+];
+// Function to calculate instrument settings based on the poses
 let instrument_settings = (i) => {
-  let n = 1
-  let calc = x => n > 0 ? x - 1 : x
+  let n = 1;
+  let calc = (x) => (n > 0 ? x - 1 : x);
 
   let [f, s, v] = [
-    calc(i.poses.faces), 
-    calc(i.poses.sides), 
-    calc(i.poses.vertical)
-  ]
-  
-  let face_option = f > 2 ? [faces[1]] 
-  : f > 1 ? [faces[0]] 
-  : f > 0 ? [faces[0], faces[1]] 
-  : []
-  let sides = s > 2 ? [faces[2]] 
-  : s > 1 ? [faces[3]] 
-  : s > 0 ? [faces[3], faces[2]] 
-  : []
-  let vertical = v > 2 ? [faces[5]]
-  : v > 1 ? [faces[4]] 
-  : v > 0 ? [faces[4], faces[5]] 
-  : []
+    calc(i.poses.faces),
+    calc(i.poses.sides),
+    calc(i.poses.vertical),
+  ];
 
-  return [...face_option, ...sides, ...vertical]
-}
+  let face_option =
+    f > 2 ? [faces[1]] : f > 1 ? [faces[0]] : f > 0 ? [faces[0], faces[1]] : [];
+  let sides =
+    s > 2 ? [faces[2]] : s > 1 ? [faces[3]] : s > 0 ? [faces[3], faces[2]] : [];
+  let vertical =
+    v > 2 ? [faces[5]] : v > 1 ? [faces[4]] : v > 0 ? [faces[4], faces[5]] : [];
 
-const cubes = ref([])
-const hl = ref([])
+  return [...face_option, ...sides, ...vertical];
+};
 
-const chng_face = (i, face) => {
-  let c = cubes.value.filter(ii => ii[0] != i)
-  let h = hl.value.filter(ii => ii[0] != i)
-  c.push([i, `show-${face}`])
-  h.push([i, face])
-  cubes.value = c
-  hl.value = h
-}
+const cubes = ref([]);
+const hl = ref([]);
+const selectedFace = ref(faces[0]);
 
-const getCube = i => {
-  let c = cubes.value.filter(ii => ii[0] == i)
-  return c.length > 0 ? c[0][1] : null
-}
+// Function to change the selected face for a cube
+const chng_face = (i) => {
+  let c = cubes.value.filter((ii) => ii[0] != i);
+  let h = hl.value.filter((ii) => ii[0] != i);
+  c.push([i, `show-${selectedFace.value}`]);
+  h.push([i, selectedFace.value]);
+  cubes.value = c;
+  hl.value = h;
+};
 
+// Function to get the current face of a cube
+const getCube = (i) => {
+  let c = cubes.value.filter((ii) => ii[0] == i);
+  return c.length > 0 ? c[0][1] : null;
+};
+
+// Function to check if a face of a cube is highlighted
 const getHL = (i, face) => {
-  let h = hl.value.filter(ii => ii[0] == i)
-  return h.length > 0 && h[0][1] == face ? true : false
-}
+  let h = hl.value.filter((ii) => ii[0] == i);
+  return h.length > 0 && h[0][1] == face ? true : false;
+};
 
+// Function to get the selected face of a cube
+const getFace = (i) => {
+  let h = hl.value.filter((ii) => ii[0] == i);
+  return h.length > 0 ? h[0][1] : null;
+};
+
+// Reactive variable for filtered entries
+const filteredEntries = ref(proto);
+
+// Reactive object for selected filters
+const selectedFilters = reactive({
+  face_up: true,
+  face_down: true,
+  right: true,
+  left: true,
+  vertical_up: true,
+  vertical_down: true,
+});
+
+// Function to filter the entries based on the selected pose filters
+const filterEntries = () => {
+  const filters = Object.keys(selectedFilters).filter(
+    (key) => selectedFilters[key]
+  );
+  filteredEntries.value = proto.filter((entry) => {
+    const settings = instrument_settings(entry.settings);
+    return filters.some((filter) => settings.includes(filter));
+  });
+};
+
+watch(selectedFilters, filterEntries, { deep: true });
+onMounted(filterEntries);
 </script>
-
 <template>
+  <div>
+    <div class="filter-checkboxes">
+      <label v-for="pose in faces" :key="pose" class="checkbox-container">
+        <input
+          type="checkbox"
+          :value="pose"
+          v-model="selectedFilters[pose]"
+          @change="filterEntries"
+        />
+        {{ pose }}
+      </label>
+    </div>
 
-  <table class="list">
-    <th>face</th>
-    <th>options</th>
-    <th>result</th>
+    <table class="list">
+      <th>face</th>
+      <th>options</th>
+      <th>result</th>
 
-    <template v-for="(item, idx) of proto">
-      
-      <tr class="title space">
-        <td colspan="3">{{ '|   ' }}</td>
-      </tr>
-      
-      <tr class="title">
-        <td colspan="3">{{ item.scalpel_id }}</td>
-      </tr>
-      
-      <tr class="v_up">
-        <td>{{ idx }}</td>
-        <td class="code">
-          poses: {{ item.settings.poses }}
-        </td>
-        <td>
-          <div v-for="res of instrument_settings(item.settings)">{{ res }}</div>
-        </td>
-      </tr>
-      
-      <tr>
+      <template v-for="(item, idx) of filteredEntries">
+        <tr class="title space">
+          <td colspan="3">{{ "|   " }}</td>
+        </tr>
 
-        <td>
-          <div class="scene">
-            <div :class="`cube ${getCube(idx)}`">
-              <div 
-              v-for="pose in faces" 
-              :class="`cube__face cube__face--${pose}${getHL(idx, pose) ? ' highlight' : ''}`">
-                {{ pose.split('_').join(' ') }}
-              </div>
+        <tr class="title">
+          <td colspan="3">{{ item.scalpel_id }}</td>
+        </tr>
+
+        <tr class="v_up">
+          <td>{{ idx }}</td>
+          <td class="code">poses: {{ item.settings.poses }}</td>
+          <td>
+            <div v-for="res of instrument_settings(item.settings)">
+              {{ res }}
             </div>
-          </div>
-        </td>
+          </td>
+        </tr>
 
-        <td>
-
-          <div style="display: flex; flex-direction: row;">
-            <div v-for="face in instrument_settings(item.settings)" class="scene2">
-              <div :class="`cube2 show-${face}`">
-                <div 
-                v-for="pose in faces" 
-                :class="`cube2__face cube2__face--${pose}`">
-                  {{ pose == face ? String(pose).split('_').join(' ') : null }}
+        <tr class="scene_container">
+          <td>
+            <div class="scene">
+              <div :class="`cube ${getCube(idx)}`">
+                <div
+                  v-for="pose in faces"
+                  :class="`cube__face cube__face--${pose}${
+                    getHL(idx, pose) ? ' highlight' : ''
+                  }`"
+                >
+                  {{ pose.split("_").join(" ") }}
                 </div>
               </div>
             </div>
-          </div>
+          </td>
 
-          <div style="display: flex; flex-direction: row;">
-            <button
-            v-for="face in faces"
-            @click="chng_face(idx, face)">
-              {{ face }}
-            </button>
-          </div>
+          <td>
+            <div style="display: flex; flex-direction: row">
+              <div class="scene2">
+                <div :class="`cube2 ${getCube(idx)}`">
+                  <div
+                    v-for="pose in faces"
+                    :class="`cube2__face cube2__face--${pose}`"
+                  >
+                    {{
+                      pose == getFace(idx)
+                        ? String(pose).split("_").join(" ")
+                        : null
+                    }}
+                  </div>
+                </div>
+              </div>
+            </div>
 
-        </td>
-      </tr>
-    
-    </template>
-  </table>
-
+            <div style="display: flex; flex-direction: row">
+              <select v-model="selectedFace" @change="chng_face(idx)">
+                <option
+                  v-for="face in instrument_settings(item.settings)"
+                  :value="face"
+                >
+                  {{ face }}
+                </option>
+              </select>
+            </div>
+          </td>
+        </tr>
+      </template>
+    </table>
+  </div>
 </template>
-
 <style scoped lang="sass">
 
 body
@@ -178,4 +231,12 @@ td
 .active
   background-color: #565ed8
 
+.cube2__face:hover
+  background-color: #c4c4ff
+
+.scene_container
+  height: 180px
+
+.blue-container:hover
+  background-color: #b0d4ff
 </style>
